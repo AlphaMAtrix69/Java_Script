8c14f329ff2d747a0635c790cc35121a3ddfd1c5
AlphaMAtrix69 <107604976+AlphaMAtrix69@users.noreply.github.com>
2023-08-25 21:24:32 +0000

    variables

---
+++ Basics_01/variables.js
@@ -0,0 +1,22 @@
+const studentId = 785545
+let studentEmail = "qwee@gmail.com"
+var studentPassword = "74512274"
+studentCity = "awsfg" //Declaring variable without keyword
+let studentState
+
+// studentId = 74
+studentEmail = "rfwq@.com"
+studentPassword = "852"
+studentCity = "jk"
+
+console.table([studentId, studentCity, studentEmail, studentPassword, studentState])
+
+
+// In JS variable declaration happen in 3 ways
+// 1) Using const keyword, can't change the assigned value
+// 2) Using let & var keyword, can change assigned value
+// 3) by just declaring & initializing variable name & value, change assigned value
+
+
+// but but so let & var are used multiple times for same thing here comes catch 
+// var has issue in block scope & functional scope.
