8416c9b4861865b3db932077c38ea0d03ae026de
AlphaMAtrix69 <107604976+AlphaMAtrix69@users.noreply.github.com>
2023-08-25 21:28:50 +0000

    datatypes

---
+++ Basics_01/datatypes.js
@@ -0,0 +1,23 @@
+//There is whole difference b/w JS of earlier version & current version,
+//earlier there were no classes modules & arrow functions & so much code of JS is coded with earlier format.
+// So to establish same standard for JS coding we use "use strict" 
+"use strict" //Treat all JS code  as newer version
+
+///working with Datatype in JS is easy
+let name = "name" //declared in " " then string
+let age = 17 //int
+let isUser = true //boolean type
+
+//Types of datatype
+//numbers 
+//bigint
+//string
+//boolean
+//null - It is value(standalone) as well as datatype, it means empty variable is declared but assigned value is null
+//Undefined - Value is not defined but variable is declared
+//Symbol - unique
+
+//object 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object 
